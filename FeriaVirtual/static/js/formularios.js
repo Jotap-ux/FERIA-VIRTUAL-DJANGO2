46c13d2e7cbdf19b9c90df1376d22d2703c9e21a
Jotap-ux <juanpablochato123@gmail.com>
2023-12-01 19:38:48 -0300

    validacion-PatenteOK

--- FeriaVirtual/static/js/formularios.js
+++ FeriaVirtual/static/js/formularios.js
@@ -28,6 +28,30 @@ $(document).ready(function () {
         $("#miFormulario").submit();
         
     });
+
+    //-------------------------------------------------------------
+    //FORMULARIO REGISTRO DE VEHICULO
+    $("#btnMostrarModalVEHICULO").click(function () {
+        if (validarFormularioVEHICULO()) {
+            // Muestra el primer modal solo si las validaciones son exitosas
+            $('#modal-registro-vehiculo').modal('show'); 
+        } else {
+            // Evita el envío del formulario si la validación falla
+            event.preventDefault();
+        }
+        //$("#modal-registro-persona").modal('hide');
+              
+        
+    });
+
+    $("#btnAceptar-agregadoVEHICULO").click(function () {
+        // Oculta el modal
+        //$("#modal-registro-persona").modal('hide');
+        
+        $("#miFormularioREGISTRO_TRANSPORTE").submit();
+        
+    });
+
 });
    
 
@@ -50,3 +74,27 @@ function validarFormulario() {
         return false;
     }
 }
+
+// Función para validar el formulario INGRESO DE VEHICULO
+function validarFormularioVEHICULO() {
+
+    var patenteRegex = /^([A-Za-z]{4}\d{2}|\d{2}[A-Za-z]{4})$/;
+    var patenteInput = $("#id_patente").val();
+
+    if (!patenteRegex.test(patenteInput)) {
+        
+        $('#modal-error-patente').modal('show');
+        return false;
+    }
+
+    if ($("#miFormularioREGISTRO_TRANSPORTE")[0].checkValidity()) {
+
+        return true; 
+
+    } else {
+        
+        $('#modal-aviso-DATOS-VEHICULO').modal('show');   
+        return false;
+    }
+}
+
